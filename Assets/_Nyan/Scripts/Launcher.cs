e1d8e51389822d69ef100cb8cafe575e7b7a239a
haohao <109795457+nyan222727@users.noreply.github.com>
2026-05-29 12:32:13 +0800

    Feat: spawn panda on AR plane via tap

--- Assets/_Nyan/Scripts/Launcher.cs
+++ Assets/_Nyan/Scripts/Launcher.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 public class Launcher : MonoBehaviour
 {
     public Rigidbody _prefabWithRigidbody;
+
     void Update()
     {
-#if UNITY_EDITOR
-        if(Input.GetMouseButtonDown(0))
-#else
-        if (Input.touchCount > 0)
-#endif
+        if (!WasTapped())
         {
-            // spawn in front of at the camera
-            var pos = Camera.main.transform.position;
-            var forw = Camera.main.transform.forward;
-            var thing = Instantiate(_prefabWithRigidbody, pos+(forw*0.4f), Quaternion.identity);
-
-            thing.AddForce(forw * 200.0f);
+            return;
         }
+
+        if (_prefabWithRigidbody == null || Camera.main == null)
+        {
+            return;
+        }
+
+        var cameraTransform = Camera.main.transform;
+        var pos = cameraTransform.position;
+        var forw = cameraTransform.forward;
+        var thing = Instantiate(_prefabWithRigidbody, pos + (forw * 0.4f), Quaternion.identity);
+
+        thing.AddForce(forw * 200.0f);
+    }
+
+    private static bool WasTapped()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+
+        if (Input.touchCount == 0)
+        {
+            return false;
+        }
+
+        return Input.GetTouch(0).phase == TouchPhase.Began;
     }
 }
---
+++ Assets/_Nyan/Scripts/PandaPlacement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PandaPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject pandaPrefab;
+    [SerializeField] private ReticleBehaviour reticle;
+    [SerializeField] private DrivingSurfaceManager drivingSurfaceManager;
+    [SerializeField] private Launcher projectileLauncher;
+    [SerializeField] private bool lockPlaneAfterPlacement = true;
+    [SerializeField] private bool hideReticleAfterPlacement = true;
+
+    private GameObject spawnedPanda;
+
+    private void Update()
+    {
+        if (spawnedPanda != null || pandaPrefab == null || reticle == null)
+        {
+            return;
+        }
+
+        if (!WasTapped() || reticle.CurrentPlane == null)
+        {
+            return;
+        }
+
+        spawnedPanda = Instantiate(pandaPrefab, reticle.transform.position, reticle.transform.rotation);
+
+        if (lockPlaneAfterPlacement && drivingSurfaceManager != null)
+        {
+            drivingSurfaceManager.LockPlane(reticle.CurrentPlane);
+        }
+
+        if (hideReticleAfterPlacement)
+        {
+            reticle.gameObject.SetActive(false);
+        }
+
+        if (projectileLauncher != null)
+        {
+            projectileLauncher.enabled = true;
+        }
+    }
+
+    private static bool WasTapped()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+
+        if (Input.touchCount == 0)
+        {
+            return false;
+        }
+
+        return Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+}
